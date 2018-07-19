b5eacb986a0b7d12aacceb8edd108fdca3f378d4
siver <2278254874@qq.com>
2018-07-19 17:26:18 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by wushuang on 2018/7/19.
  */
-import ReactDomPrinter from 'src/ReactDomPrinter'
+import ReactDomPrinter from 'ReactDomPrinter'
 
 module.exports = {
     ReactDomPrinter
